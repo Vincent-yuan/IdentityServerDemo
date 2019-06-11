53b1ab3375aa14097dc0f7f920034923abbf10a6
yuan.neuq@foxmail.com <yuan.neuq@foxmail.com>
2019-06-11 23:52:59 +0800

    IdentityServer4简单使用

---
+++ APIService1/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace APIService1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //IdentityServer
+            services.AddMvcCore().AddAuthorization().AddJsonFormatters();
+            services.AddAuthentication(Configuration["Identity:Scheme"])
+                        .AddIdentityServerAuthentication(options =>
+                        {
+                            options.RequireHttpsMetadata = false; //是否需要https
+                            options.Authority = $"http://{Configuration["Identity:IP"]}:{Configuration["Identity:Port"]}";  //IdentityServer授权路径
+                            options.ApiName = Configuration["Service:Name"];  //需要授权的服务名称
+                        });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //启用Authentication中间件
+            app.UseAuthentication();
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ IdentityServerDemo/InMemoryConfiguration.cs
@@ -0,0 +1,114 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServerDemo
+{
+    public class InMemoryConfiguration
+    {
+        public static IConfiguration Configuration { get; set; }
+        /// <summary>
+        /// Define which APIs will use this IdentityServer
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource("clientservice", "CAS Client Service"),
+                new ApiResource("productservice", "CAS Product Service"),
+                new ApiResource("agentservice", "CAS Agent Service")
+            };
+        }
+
+        /// <summary>
+        /// Define which Apps will use thie IdentityServer
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client
+                {
+                    ClientId = "client.api.service",
+                    ClientSecrets = new [] { new Secret("clientsecret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "clientservice" }
+                },
+                new Client
+                {
+                    ClientId = "product.api.service",
+                    ClientSecrets = new [] { new Secret("productsecret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "clientservice", "productservice" }
+                },
+                new Client
+                {
+                    ClientId = "agent.api.service",
+                    ClientSecrets = new [] { new Secret("agentsecret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = new [] { "agentservice", "clientservice", "productservice" }
+                },
+                new Client
+                {
+                    ClientId = "cas.mvc.client.implicit",
+                    ClientName = "CAS MVC Web App Client",
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    RedirectUris = { $"http://localhost:56458/signin-oidc" },
+                    PostLogoutRedirectUris = { $"http://localhost:56458/signout-callback-oidc" },
+                    AllowedScopes = new [] {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "agentservice", "clientservice", "productservice"
+                    },
+                    AllowAccessTokensViaBrowser = true // can return access_token to this client
+                },
+            };
+        }
+
+        /// <summary>
+        /// Define which uses will use this IdentityServer
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<TestUser> GetUsers()
+        {
+            return new[]
+            {
+                new TestUser
+                {
+                    SubjectId = "10001",
+                    Username = "test1@hotmail.com",
+                    Password = "test1password"
+                },
+                new TestUser
+                {
+                    SubjectId = "10002",
+                    Username = "test2@hotmail.com",
+                    Password = "test2password"
+                },
+                new TestUser
+                {
+                    SubjectId = "10003",
+                    Username = "test3@hotmail.com",
+                    Password = "test3password"
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+    }
+}
